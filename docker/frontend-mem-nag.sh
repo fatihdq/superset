a894b37ff7133f6a7e73eb7bb3c11d6f45fc8e8c
fatih.dq19 <samgong23@gmail.com>
2024-11-27 17:11:20 +0700

    backup

--- docker/frontend-mem-nag.sh
+++ docker/frontend-mem-nag.sh
@@ -20,14 +20,14 @@ set -e
 
 # We need at least 3GB of free mem...
 MIN_MEM_FREE_GB=3
-MIN_MEM_FREE_KB=$(($MIN_MEM_FREE_GB*1000000))
+MIN_MEM_FREE_KB=$(($MIN_MEM_FREE_GB * 1000000))
 
 echo_mem_warn() {
-  MEM_FREE_KB=$(awk '/MemFree/ { printf "%s \n", $2 }' /proc/meminfo)
-  MEM_FREE_GB=$(awk '/MemFree/ { printf "%s \n", $2/1024/1024 }' /proc/meminfo)
+    MEM_FREE_KB=$(awk '/MemFree/ { printf "%s \n", $2 }' /proc/meminfo)
+    MEM_FREE_GB=$(awk '/MemFree/ { printf "%s \n", $2/1024/1024 }' /proc/meminfo)
 
-  if [[ "${MEM_FREE_KB}" -lt "${MIN_MEM_FREE_KB}" ]]; then
-    cat <<EOF
+    if [[ "${MEM_FREE_KB}" -lt "${MIN_MEM_FREE_KB}" ]]; then
+        cat <<EOF
     ===============================================
     ========  Memory Insufficient Warning =========
     ===============================================
@@ -40,9 +40,9 @@ echo_mem_warn() {
     ========  Memory Insufficient Warning =========
     ===============================================
 EOF
-  else
-    echo "Memory check Ok [${MEM_FREE_GB}GB free]"
-  fi
+    else
+        echo "Memory check Ok [${MEM_FREE_GB}GB free]"
+    fi
 }
 
 # Always nag if they're low on mem...
